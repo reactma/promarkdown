7bc8a66c7a94a2b6afff7a7c4305ee063fef28d7
stephenye <stephenye@hotmail.com>
2019-02-18 14:56:51 +0800

    added typedoc

---
+++ README.md
@@ -0,0 +1,13 @@
+# ProMarkdown
+
+** Just initialized. Come back in a week **
+
+A professional grade markdown editor for React, based on CodeMirror.
+
+It provides professional grade editing functions for coders and tech-writers:
+
+- Key map, vim / sublime / emacs
+- Line numbers
+- Search and replace
+- Block folding
+- YAML frontmatter, the one feature motivates us to develop this component
---
+++ docs/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"1":"External module","64":"Function"},"rows":[{"id":0,"kind":1,"name":"\"index\"","url":"modules/_index_.html","classes":"tsd-kind-external-module"},{"id":1,"kind":64,"name":"Greeter","url":"modules/_index_.html#greeter","classes":"tsd-kind-function tsd-parent-kind-external-module","parent":"\"index\""},{"id":2,"kind":1,"name":"\"Greeter.test\"","url":"modules/_greeter_test_.html","classes":"tsd-kind-external-module"}]};
