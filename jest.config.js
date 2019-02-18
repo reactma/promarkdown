c416f9051c8880cab95a4fe1921f02c8148a5f70
stephenye <stephenye@hotmail.com>
2019-02-18 14:26:33 +0800

    initial config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  transform: {
+    '^.+\\.(t|j)sx?$': 'ts-jest'
+  },
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/*.test.(js|jsx|ts|tsx)"],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+}
---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none'
+}
