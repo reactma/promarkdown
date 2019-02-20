3dca9aceb582d25ff02b6283734227da672e9d93
reactma <github@reactma.com>
2019-02-20 17:28:04 +0800

    use local frontmatter modules instead of codemirror-frontmatter package for @types/codemirror definition

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var React = require("react");
-var Greeter = function (_a) {
-    var name = _a.name;
-    return <div> {"Hello " + name}</div>;
-};
-exports["default"] = Greeter;
